f0cb29fbfc515d309e2b1a5d5572de6972b03574
bhu <poochit1996@hotmail.com>
2019-10-31 13:13:10 +0700

    data v.2

--- src/main/java/com/cpe/backend/BackendApplication.java
+++ src/main/java/com/cpe/backend/BackendApplication.java
@@ -24,13 +24,56 @@ public class BackendApplication {
 			RiderRepository riderRepository,
 			RestaurantRepository restaurantRepository,
 			OptionRepository optionRepository,
-			ServiceRepository serviceRepository
+			ServiceRepository serviceRepository,
+			MainCourseRepository mainCourseRepository,
+			TypeFoodRepository typeFoodRepository,
+			TypeMenuRepository typeMenuRepository,
+			RawMaterialRepository rawMaterialRepository,
+			MenuRepository	menuRepository,
+			OrderRepository orderRepository,
+			OnOptionRepository	onOptionRepository
 	) {
 		return args -> {
+			Stream.of("นางสาวใจดี ใจกว้าง", "นายขวัญเอย ขวัญมา").forEach(name -> {
+				Service service = new Service();
+				service.setName(name);
+				serviceRepository.save(service); // บันทึก Objcet ชื่อ Customer
+			});
+
+			Stream.of("นายโจ๊ก ปลาแดก", "นายฟลุ๊คฟลุ๊ค บาท").forEach(name -> {
+				Rider rider = new Rider();
+				rider.setName(name);
+				riderRepository.save(rider); // บันทึก Objcet ชื่อ Customer
+			});
+
+			Stream.of("ร้านอาหารจานเเดียว", "ร้านเครื่องดื่ม").forEach(name -> {
+				TypeFood typeFood = new TypeFood();
+				typeFood.setName(name);
+				typeFoodRepository.save(typeFood); // บันทึก Objcet ชื่อ Customer
+			});
+
 			Stream.of("ลองดูเดะ", "กะเพราปั๊มหลังมอ", "ชาขุนพล").forEach(name -> {
 				Restaurant res = new Restaurant(); // สร้าง Object Customer
 				res.setName(name); // set ชื่อ (name) ให้ Object ชื่อ Customer
 				restaurantRepository.save(res); // บันทึก Objcet ชื่อ Customer
+			});
+
+			Stream.of("ข้าว", "ปั่น", "เย็น").forEach(name -> {
+				MainCourse mainCourse = new MainCourse();
+				mainCourse.setName(name);
+				mainCourseRepository.save(mainCourse); // บันทึก Objcet ชื่อ Customer
+			});
+
+			Stream.of("ต้ม", "ผัด").forEach(name -> {
+				TypeMenu typeMenu = new TypeMenu();
+				typeMenu.setName(name);
+				typeMenuRepository.save(typeMenu); // บันทึก Objcet ชื่อ Customer
+			});
+
+			Stream.of("หมู", "ไก่", "หมึก", "ปลา").forEach(name -> {
+				RawMaterial rawMaterial = new RawMaterial();
+				rawMaterial.setName(name);
+				rawMaterialRepository.save(rawMaterial); // บันทึก Objcet ชื่อ Customer
 			});
 
 			Stream.of("ไข่ดาว", "ไข่เจียว").forEach(name -> {
@@ -39,14 +82,26 @@ public class BackendApplication {
 				optionRepository.save(option); // บันทึก Objcet ชื่อ Customer
 			});
 
-			Stream.of("ใจดี ใจกว้าง").forEach(name -> {
-				Service service = new Service();
-				service.setName(name);
-				serviceRepository.save(service); // บันทึก Objcet ชื่อ Customer
+			Stream.of("ไข่ดาว", "ไข่เจียว").forEach(name -> {
+				Menu menu = new Menu(); // สร้าง Object Customer
+				menu.setName(name); // set ชื่อ (name) ให้ Object ชื่อ Customer
+				menuRepository.save(menu); // บันทึก Objcet ชื่อ Customer
+			});
+
+			Stream.of("ลูกค้า1").forEach(name -> {
+				Order order = new Order(); // สร้าง Object Customer
+				order.setCusName(name); // set ชื่อ (name) ให้ Object ชื่อ Customer
+				orderRepository.save(order); // บันทึก Objcet ชื่อ Customer
 			});
 
 
-
+//			OnOption onOption = new OnOption(); // สร้าง Object Customer
+//			onOption.setAmoutMenu(30); // set ชื่อ (name) ให้ Object ชื่อ Customer
+//			onOption.setAmoutOption(5);
+//			onOption.setTotalPirceMenu(30);
+//			onOption.setTotalPirceOp(5);
+//			onOption.setMenu();
+//			orderRepository.save(order); // บันทึก Objcet ชื่อ Customer
 		};
 	}
 
